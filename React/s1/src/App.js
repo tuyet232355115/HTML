1221f5ad469a5cfd3749ccf0a040c82f9eba4c2a
tuyetkung <tuyetkung@gmail.com>
2024-11-15 09:59:03 +0700

    React

---
+++ React/s1/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <div className="intro">
+          Hello T1.2409.M0
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
